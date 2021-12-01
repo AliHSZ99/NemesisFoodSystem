e556fd1822e7492c56a4c53ee8ebce53c4a3fa9c
MariaRamlochan <60351622+MariaRamlochan@users.noreply.github.com>
2021-11-30 21:20:13 -0500

    Added prospective item add, edit and delete

--- app/controllers/Item.php
+++ app/controllers/Item.php
@@ -132,6 +132,121 @@ class Item extends \app\core\Controller {
         $Item->delete($item_id);
         header('location:/User/food');
     }
+	
+	
+    // Prospective Menu Functions //
+    public function addProspectiveItem() {
+
+        if(isset($_POST['action'])){
+            if ($_FILES['newPicture']['size']>0) {
+                $check = getimagesize($_FILES['newPicture']['tmp_name']);
+                $mime_type_to_extension = ['image/jpeg'=>'.jpg',
+                                            'image/gif'=>'.gif',
+                                            'image/bmp'=>'.bmp',
+                                            'image/png'=>'.png'
+                                            ];
+                if($check !== false && isset($mime_type_to_extension[$check['mime']])){
+                    $extension = $mime_type_to_extension[$check['mime']];
+                }else{
+                    $this->view('Admin/Food/food', ['error'=>"Bad file type",'pictures'=>[]]);
+                    return;
+                }
+                
+                $filename = uniqid().$extension;
+                $filepath = $this->folder.$filename;
+
+                if($_FILES['newPicture']['size'] > 4000000){
+                     $this->view('Admin/Food/food', ['error'=>"File too large",'pictures'=>[]]);
+                     return;
+                }
+                if(move_uploaded_file($_FILES['newPicture']['tmp_name'], $filepath)){
+                    $item = new \app\models\Item();
+                    $item->item_name = $_POST['item_name'];
+                    $item->type = "prospective";
+                    $item->item_description = $_POST['item_description'];
+		    $item->goal = $_POST['goal'];
+                    $item->item_price = $_POST['item_price'];
+                    $item->item_quantity = $_POST['item_quantity'];
+                    $item->filename = "/".$this->folder.$filename;
+                    $item->insertProspectiveItem();
+
+                    header('location:/Admin/index');
+                } else{
+                    echo "There was an error";
+                } 
+            }
+        } else {
+            $picture = new \app\models\Item();
+            $pictures = $picture->getFoodItems();
+            $this->view('Admin/Prospective/addProspectiveItem', ['error'=>null, 'picture'=>$pictures]);
+        }
+    }
+
+    public function editFoodItem($item_id) {
+
+        $item = new \app\models\Item();
+        $item = $item->get($item_id);
+
+        if(isset($_POST['action'])){
+            if ($_FILES['newPicture']['size']>0) {
+                $check = getimagesize($_FILES['newPicture']['tmp_name']);
+                $mime_type_to_extension = ['image/jpeg'=>'.jpg',
+                                            'image/gif'=>'.gif',
+                                            'image/bmp'=>'.bmp',
+                                            'image/png'=>'.png'
+                                            ];
+                if($check !== false && isset($mime_type_to_extension[$check['mime']])){
+                    $extension = $mime_type_to_extension[$check['mime']];
+                }else{
+                    $this->view('Admin/Food/food', ['error'=>"Bad file type",'pictures'=>[]]);
+                    return;
+                }
+                
+                $filename = uniqid().$extension;
+                $filepath = $this->folder.$filename;
+
+                if($_FILES['newPicture']['size'] > 4000000){
+                     $this->view('Admin/Food/food', ['error'=>"File too large",'pictures'=>[]]);
+                     return;
+                }
+                if(move_uploaded_file($_FILES['newPicture']['tmp_name'], $filepath)){
+                    $item = new \app\models\Item();
+                    $item->item_name = $_POST['item_name'];
+                    $item->item_description = $_POST['item_description'];
+		    $item->goal = $_POST['goal'];
+                    $item->item_price = $_POST['item_price'];
+                    $item->item_quantity = $_POST['item_quantity'];
+                    $item->filename = "/".$this->folder.$filename;
+                    $item->item_id = $item_id;
+                    $item->editFoodItem();
+
+                     header('location:/User/food');
+                } else{
+                    echo "There was an error";
+                } 
+            } else {
+                    $item = new \app\models\Item();
+                    $item = $item->get($item_id);
+                    $item->item_name = $_POST['item_name'];
+                    $item->item_description = $_POST['item_description'];
+		    $item->goal = $_POST['goal']; 
+                    $item->item_price = $_POST['item_price'];
+                    $item->item_quantity = $_POST['item_quantity'];
+                    $item->item_id = $item_id;
+                    $item->editFoodItem();
+
+                    header('location:/User/food');
+            }
+
+        }else //1 present a form to the user
+            $this->view('Admin/Prospective/editProspectiveItem', ['item'=>$item]);
+    }
+	
+	public function deleteProspectiveItem($item_id) {
+        $Item = new \app\models\Item();
+        $Item->delete($item_id);
+        header('location:/User/food');
+    }
 
     public function editShoppingItem($item_id) {
         $item = new \app\models\Item();
