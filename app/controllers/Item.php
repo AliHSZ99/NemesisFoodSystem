f5b8653a288f282f528e4507220514d58499e97e
MariaRamlochan <60351622+MariaRamlochan@users.noreply.github.com>
2021-11-30 21:02:09 -0500

    Updated add, edit and delete food item

--- app/controllers/Item.php
+++ app/controllers/Item.php
@@ -26,7 +26,7 @@ class Item extends \app\core\Controller {
     public function addFoodItem() {
 
         if(isset($_POST['action'])){
-            if (isset($_FILE['newPicture'])) {
+            if ($_FILES['newPicture']['size']>0) {
                 $check = getimagesize($_FILES['newPicture']['tmp_name']);
                 $mime_type_to_extension = ['image/jpeg'=>'.jpg',
                                             'image/gif'=>'.gif',
@@ -54,11 +54,10 @@ class Item extends \app\core\Controller {
                     $item->item_description = $_POST['item_description'];
                     $item->item_price = $_POST['item_price'];
                     $item->item_quantity = $_POST['item_quantity'];
-                    $item->goal = $_POST['goal'];
-                    //$item->filename = "/".$this->$folder.$filename;
+                    $item->filename = "/".$this->folder.$filename;
                     $item->insertFoodItem();
 
-                    header('location:/User/food');
+                    header('location:/Admin/Food/food');
                 } else{
                     echo "There was an error";
                 } 
@@ -70,22 +69,68 @@ class Item extends \app\core\Controller {
         }
     }
 
-    public function editFoodItem() {
-        if(isset($_POST['action'])){
-            $item = new \app\models\Item();
-            $item->item_name = $_POST['item_name'];
-            $item->type = "food";
-            $item->item_description = $_POST['item_description'];
-            $item->item_price = $_POST['item_price'];
-            $item->item_quantity = $_POST['item_quantity'];
-            $item->goal = $_POST['goal'];
-            $item->filename = $_POST['filename'];
-            $item->editFoodItem();
+    public function editFoodItem($item_id) {
 
-            header('location:/User/food');
+        $item = new \app\models\Item();
+        $item = $item->get($item_id);
+
+        if(isset($_POST['action'])){
+            if ($_FILES['newPicture']['size']>0) {
+                $check = getimagesize($_FILES['newPicture']['tmp_name']);
+                $mime_type_to_extension = ['image/jpeg'=>'.jpg',
+                                            'image/gif'=>'.gif',
+                                            'image/bmp'=>'.bmp',
+                                            'image/png'=>'.png'
+                                            ];
+                if($check !== false && isset($mime_type_to_extension[$check['mime']])){
+                    $extension = $mime_type_to_extension[$check['mime']];
+                }else{
+                    $this->view('Admin/Food/food', ['error'=>"Bad file type",'pictures'=>[]]);
+                    return;
+                }
+                
+                $filename = uniqid().$extension;
+                $filepath = $this->folder.$filename;
+
+                if($_FILES['newPicture']['size'] > 4000000){
+                     $this->view('Admin/Food/food', ['error'=>"File too large",'pictures'=>[]]);
+                     return;
+                }
+                if(move_uploaded_file($_FILES['newPicture']['tmp_name'], $filepath)){
+                    $item = new \app\models\Item();
+                    $item->item_name = $_POST['item_name'];
+                    $item->item_description = $_POST['item_description'];
+                    $item->item_price = $_POST['item_price'];
+                    $item->item_quantity = $_POST['item_quantity'];
+                    $item->filename = "/".$this->folder.$filename;
+                    $item->item_id = $item_id;
+                    $item->editFoodItem();
+
+                     header('location:/User/food');
+                } else{
+                    echo "There was an error";
+                } 
+            } else {
+                    $item = new \app\models\Item();
+                    $item = $item->get($item_id);
+                    $item->item_name = $_POST['item_name'];
+                    $item->item_description = $_POST['item_description'];
+                    $item->item_price = $_POST['item_price'];
+                    $item->item_quantity = $_POST['item_quantity'];
+                    $item->item_id = $item_id;
+                    $item->editFoodItem();
+
+                    header('location:/User/food');
+            }
 
         }else //1 present a form to the user
-            $this->view('Admin/Food/editFoodItem');
+            $this->view('Admin/Food/editFoodItem', ['item'=>$item]);
+    }
+	
+	public function deleteFoodItem($item_id) {
+        $Item = new \app\models\Item();
+        $Item->delete($item_id);
+        header('location:/User/food');
     }
 
     public function editShoppingItem($item_id) {
